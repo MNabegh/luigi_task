20d514f3b0200d45cfa79270fd80d5898d2b7a55
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2023-06-19 21:44:02 +0200

    add check data cleaning checks

--- tasks/data_cleaning_task.py
+++ tasks/data_cleaning_task.py
@@ -29,8 +29,8 @@ class DataCleaningTask(luigi.Task):
         self.read_data()
 
         # Data Validation: Checks for corrupted data that might affect the logic later on
-        expected_schema = pd.Series({'userId': np.dtype(int), 'id': np.dtype(
-            int), 'title': np.dtype('O'), 'body': np.dtype('O')})
+        expected_schema = pd.Series({"userId": np.dtype(int), "id": np.dtype(
+            int), "title": np.dtype("O"), "body": np.dtype("O")})
         self.validate_schema(expected_schema)
 
         str_cols = ["title", "body"]
@@ -40,6 +40,13 @@ class DataCleaningTask(luigi.Task):
         self.validate_int_values(int_cols)
 
         # Data completness and consistency
+        self.check_and_fill_na()
+
+        subset = ["userId", "id"]
+        self.check_and_drop_duplicates(subset)
+
+        columns_to_lower = ["title", "body"]
+        self.transform_all_strings_to_lower_case(columns_to_lower)
 
         self.write_output()
 
@@ -54,7 +61,7 @@ class DataCleaningTask(luigi.Task):
             # Exception raised to indicate that we can not continue operating on this data
             raise Exception("Illegal Argument data schema mismatch")
 
-    def validate_str_values(self, str_cols: List[str]):
+    def validate_str_values(self, str_cols: List[str]) -> bool:
         is_valid = True
         for col in str_cols:
             if not self.is_string_series(self.df[col]):
@@ -65,7 +72,7 @@ class DataCleaningTask(luigi.Task):
 
         return is_valid
 
-    def validate_int_values(self, int_cols: List[str]):
+    def validate_int_values(self, int_cols: List[str]) -> bool:
         is_valid = True
         for col in int_cols:
             if not (self.df[col] < 11).all():
@@ -75,15 +82,44 @@ class DataCleaningTask(luigi.Task):
 
         return is_valid
 
-    def is_string_series(self, s: pd.Series):
+    def is_string_series(self, s: pd.Series) -> bool:
         if isinstance(s.dtype, pd.StringDtype):
             # The series was explicitly created as a string series (Pandas>=1.0.0)
             return True
-        elif s.dtype == 'object':
+        elif s.dtype == "object":
             # Object series, check each value
             return all((v is None) or isinstance(v, str) for v in s)
         else:
             return False
+
+    def check_and_fill_na(self) -> pd.DataFrame:
+        na_indicies = np.where(self.df.isna().any(axis=1))[0]
+
+        logger.debug(f"{na_indicies} these rows contain NA values")
+
+        # Fill NaN values based on column type
+        for col in self.df.columns:
+            if self.df[col].dtype == "object":
+                self.df.loc[self.df[col].isna(), col] = self.df.loc[self.df[col].isna()
+                                                                    ].fillna("Unknown")
+            elif self.df[col].dtype == "int64" or self.df[col].dtype == "float64":
+                self.df.loc[self.df[col].isna(
+                ), col] = self.df.loc[self.df[col].isna()].fillna(0)
+
+    def check_and_drop_duplicates(self, subset: List[str]):
+        # Check if the combination of subset columns is unique
+        is_unique = ~self.df.duplicated(subset=subset)
+
+        duplicated_indices = np.where(~is_unique)[0]
+
+        logger.debug(f"{duplicated_indices} these rows contained NA values")
+
+        # Drop the duplicated combinations
+        self.df = self.df[is_unique]
+
+    def transform_all_strings_to_lower_case(self, columns_to_lower: List[str]):
+        self.df[columns_to_lower] = self.df[columns_to_lower].apply(
+            lambda x: x.str.lower())
 
     def write_output(self):
         self.df.to_parquet(self.output().path)
