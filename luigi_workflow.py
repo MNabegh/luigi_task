65fbcf5fd5d1683eec97d53a8a96e75949e0f8ca
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2023-06-20 19:44:16 +0200

    change Luigi workflow to load the data into the database

--- luigi_workflow.py
+++ luigi_workflow.py
@@ -1,6 +1,8 @@
 import luigi
 
-from tasks.data_transformation_task import DataTransformationTask
+from tasks.create_db_task import CreateDatabaseTask
+from tasks.create_table_task import CreateTableTask
+from tasks.load_data_into_db_task import LoadDataToDatabaseTask
 
 
 class LuigiWorkflow(luigi.WrapperTask):
@@ -10,7 +12,7 @@ class LuigiWorkflow(luigi.WrapperTask):
     transformation_path = luigi.Parameter()
 
     def requires(self):
-        return DataTransformationTask(
+        return LoadDataToDatabaseTask(
             self.url_to_json_file,
             self.json_path,
             self.staging_path,
